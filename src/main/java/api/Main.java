87f3ef329fbee7297990676de67fc998c3cc66f3
Deniz <55638248+denizoral@users.noreply.github.com>
2021-11-29 03:04:43 +0000

    Update Main.java

--- src/main/java/api/Main.java
+++ src/main/java/api/Main.java
@@ -2,7 +2,7 @@ package api;
 
 public class Main {
     public static void main(String[] args) {
-        WeatherApi weatherApi = new WeatherApi("Ankara");
+        WeatherApi weatherApi = new WeatherApi("London"); //for testing purposes
         weatherApi.apiCall();
     }
 }
